7892ff560743bd185ec1869eca2f85c3ffb2a3b9
Maxim <maksukh@inbox.ru>
2021-04-18 22:58:04 +0300

    Add mobile] support

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,13 +9,18 @@ export default class Home extends React.Component {
             <div className="Home">
                 <div id="bg"/>
                 <div className={styles.menu}>
-                    <h1>Travel map</h1><br/><br/>
-                    <NavLink to="/login">
-                        <button className={styles.loginBtn}>Login</button>
-                    </NavLink>
-                    <br/><br/><br/><br/>
-                    <NavLink to="/registration">
-                        <button className={styles.registerBtn}>Register</button>
+                    <h1>Travel map</h1>
+                    <div className={styles.loginLinkContainer}>
+                        <NavLink className={styles.link} to="/login">
+                            <button className={styles.loginBtn}>
+                                Login
+                            </button>
+                        </NavLink>
+                    </div>
+                    <NavLink className={styles.link} to="/registration">
+                        <button className={styles.registerBtn}>
+                            Register
+                        </button>
                     </NavLink>
                 </div>
             </div>
