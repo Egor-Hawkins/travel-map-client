3dfc258b08482137dd0508892def72ccaad4cdd0
Maxim <maksukh@inbox.ru>
2021-04-29 23:37:11 +0300

    Change sidebar settings and styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ import {NavLink} from "react-router-dom";
 export default class Sidebar extends React.Component {
     render() {
         return (
-            <Menu>
+            <Menu width={"280px"} disableAutoFocus>
                 <NavLink className="menu-item" to="/">Home</NavLink>
                 <NavLink className="menu-item" to="/registration">Register</NavLink>
                 <NavLink className="menu-item" to="/login">Login</NavLink>
