e6480e606413c43aa346f470428c1e9b8f376fd3
Maxim <maksukh@inbox.ru>
2021-05-14 06:49:46 +0300

    Edit dependencies and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,21 +10,17 @@ export default class Home extends React.Component {
             <div className="Home">
                 <Sidebar/>
                 <div id="bg"/>
-                <div className={styles.centered}>
-                    <h1 className={styles.header}>Travel map</h1>
-                    <div className={styles.loginLinkContainer}>
-                        <NavLink className={styles.link} to="/login">
-                            <button className={styles.loginBtn}>
-                                Login
-                            </button>
-                        </NavLink>
-                    </div>
-                    <NavLink className={styles.link} to="/registration">
-                        <button className={styles.registerBtn}>
-                            Register
-                        </button>
-                    </NavLink>
-                </div>
+                <h1 className={styles.header}>Travel map</h1>
+                <NavLink className={styles.link} to="/login">
+                    <button className={styles.loginBtn}>
+                        Login
+                    </button>
+                </NavLink>
+                <NavLink className={styles.link} to="/registration">
+                    <button className={styles.registerBtn}>
+                        Register
+                    </button>
+                </NavLink>
             </div>
         );
     }
